425d1b6ad2ce6af66c08b191b1ece1034d34955d
Roberto Toapanta <robertocarlos.toapanta@gmail.com>
2025-09-05 14:29:53 -0500

    zabbix sender

--- config/station_mapping.py
+++ config/station_mapping.py
@@ -4,6 +4,10 @@ Mapping from station number to station name.
 
 STATION_NAMES = {
     1: "VC1",
+    5: "GPCAM",
+    6: "CAYR",
+    7: "CAYM",
+    9: "COTOR",
     11: "GGPA",
     # Add other station mappings here
 }
