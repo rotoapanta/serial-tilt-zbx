b22144409c8f1eb11b2598f6ba473bfe98a5f51c
Roberto Toapanta <robertocarlos.toapanta@gmail.com>
2025-09-05 14:36:44 -0500

    update stations

--- config/station_mapping.py
+++ config/station_mapping.py
@@ -4,10 +4,15 @@ Mapping from station number to station name.
 
 STATION_NAMES = {
     1: "VC1",
+    2: "BILBAO",
+    3: "RETU",
+    4: "CHONTAL",
     5: "GPCAM",
     6: "CAYR",
     7: "CAYM",
+    8: "PONDOA",
     9: "COTOR",
+    10: "MANDUR"    # cambiar en estacion
     11: "GGPA",
     # Add other station mappings here
 }
